49f25d89f22796af72f025a099587111c2ec1884
Badawi7 <badawi2005@hotmail.com>
2021-11-09 10:52:58 +0200

    renderTemplate introduced
    The function renderTemplate was introduced to perform string interpolation manually.

--- App/main.js
+++ App/main.js
@@ -1,12 +1,8 @@
 $(document).ready(init);
 
 function init() {
+  renderTable();
   $('.action-link').click(onActionLinkClick); //Applies to any button responsible for routing
-
-  //Temporary (start)
-  navigate('.patients-list');
-  renderTable($('.patients-list table'), patientsData);
-  //Temporary (end)
 }
 
 function onActionLinkClick(event) {
@@ -29,16 +25,32 @@ function hideAll() {
 function renderTable() {
   const tableBodyEl = $('.patients-list table tbody');
   tableBodyEl.empty(); //Clear the table first
+  const rowTemplate = '<tr><td>{{ID}}</td>' +
+  '<td>{{fname}} {{mname}} {{lname}}</td>' +
+  '<td>{{email}}</td>' +
+  '<td>{{gender}}</td>' +
+  '<td>{{DOB}}</td>' +
+  '<td>{{Active}}</td>' +
+  '<td>By {{CreatedBy}} at {{creationDate}}</td>' +
+  '<td><button type="button" class="btn btn-outline-secondary action-link" ' +
+  'data-target=".patient-edit">Edit</button></td></tr>';
 
   patientsData.forEach(function (record) {
-    const rowHTML = `<tr><td>${record.ID}</td>
-    <td>${record.fname} ${record.mname} ${record.lname}</td>
-    <td>${record.email}</td>
-    <td>${record.gender}</td>
-    <td>${record.DOB}</td>
-    <td>${record.Active}</td>
-    <td>By ${record.CreatedBy} at ${record.creationDate}</td>
-    <td><button type="button" class="btn btn-outline-secondary router-link" data-target=".patient-edit">Edit</button></td></tr>`;
+    const rowHTML = renderTemplate(rowTemplate, record);
     tableBodyEl.append(rowHTML);
   });
+}
+
+function renderTemplate(templateText, data) {
+  let result = templateText;
+  let currentPosition = result.indexOf('{{');
+  while (currentPosition > -1) {
+    const end = result.indexOf('}}', currentPosition + 2); //Start searching after the opening braces
+    const placeholder = result.substring(currentPosition, end + 2);
+    const key = result.substring(currentPosition + 2, end);
+    result = result.replace(placeholder, data[key]);
+
+    currentPosition = result.indexOf('{{', currentPosition); //Start searching from the current position
+  }
+  return result;
 }
