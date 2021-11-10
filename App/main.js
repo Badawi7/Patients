a1fbe7bcd4e4182e012c03fb1a47b640a2dcc6ae
Badawi7 <badawi2005@hotmail.com>
2021-11-10 19:45:30 +0200

    Patient edit functionality implemented (card 11)
    Implemented patient editing functionality (filling form with patient info and saving back to the corresponding object).

--- App/main.js
+++ App/main.js
@@ -1,9 +1,13 @@
+let currentPatient;
+let genderRNL;
+
 $(document).ready(init);
 
 function init() {
-  renderTable();
+  genderRNL = $('.patient-edit form')[0].elements['gender'];
   $('.action-link').click(onActionLinkClick); //Applies to any button responsible for routing
-  $('.patient-edit-btn').click(onPatientEditClick);
+  $('.patient-edit form').on('submit', onPatientEditFormSubmit);
+  renderTable();
 }
 
 function onActionLinkClick(event) {
@@ -32,6 +36,9 @@ function renderTable() {
     const rowHTML = renderTemplate(rowTemplate, record);
     tableBodyEl.append(rowHTML);
   });
+
+  tableBodyEl.find('.action-link').click(onActionLinkClick);
+  $('.patient-edit-btn').click(onPatientEditClick);
 }
 
 function renderTemplate(templateText, data) {
@@ -43,4 +50,27 @@ function renderTemplate(templateText, data) {
 function onPatientEditClick(event) {
   const patientID = $(event.target).data('id');
   $('.patient-id').html(patientID);
+  currentPatient = patientsData.find(patient => patient.ID == patientID);
+  $('#fname-field').val(currentPatient.fname);
+  $('#mname-field').val(currentPatient.mname);
+  $('#lname-field').val(currentPatient.lname);
+  genderRNL.value = currentPatient.gender;
+  $('#dob-field')[0].valueAsDate = currentPatient.DOB;
+  $('#email-field').val(currentPatient.email);
+  $('#last-check-field')[0].valueAsDate = currentPatient.lastCheck;
+  $('#status-field').val(currentPatient.status);
+}
+
+function onPatientEditFormSubmit(event) {
+  currentPatient.fname = $('#fname-field').val();
+  currentPatient.mname = $('#mname-field').val();
+  currentPatient.lname = $('#lname-field').val();
+  currentPatient.gender = genderRNL.value;
+  currentPatient.DOB = $('#dob-field')[0].valueAsDate;
+  currentPatient.email = $('#email-field').val();
+  currentPatient.lastCheck = $('#last-check-field')[0].valueAsDate;
+  currentPatient.status = $('#status-field').val();
+  
+  renderTable();
+  event.preventDefault(); //Prevent submitting the form
 }
